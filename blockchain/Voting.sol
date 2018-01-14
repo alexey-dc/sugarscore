107eaf2ccd360435e3e94f0bc05bc2164aeea28c
Alexey Chernikov <iwizem@gmail.com>
2018-01-13 21:33:23 -0800

    Data structures for the sol script

--- blockchain/Voting.sol
+++ blockchain/Voting.sol
@@ -1,20 +1,43 @@
 // We have to specify what version of compiler this code will compile with
 pragma solidity ^0.4.11;
+  // - getProfile(address)
+  //   - coinsIn (int)
+  //   - totalBorrowed (int)
+  //   - borrowLimit (int)
+  //   - reputation (int)
+  // - getLoans
+  //   - loans (array)
+  //     [
+  //       - loanId (int)
+  //       - paybackAmount (int)
+  //       - daysRemaining (int)
+  //     ]
+  // - borrow(address, amount, ratePercent, durationDays)
+  // - payBack(address, loanId)
 
 contract Ibcs {
 
+  uint256 loanIndex = 0;
   struct Loan {
-    uint amount;
+    uint256 loanId;
+    uint256 amount;
+    uint32 rate; // decimals not supported in solidity; say 100% = 10000 rate, 0.1% = 10 rate
+    uint32 origination; // Unix timestamp
+    uint32 duration; // Milliseconds
     address borrower;
-    uint rate; // decimals not supported in solidity; say 100% = 10000 rate, 0.1% = 10 rate
-    uint year;
-    uint month;
-    uint day;
-    uint durationDays;
+  }
+
+  struct LoanTransaction {
+    uint256 loanId;
+    uint32 repayTimestamp; // 0 for missed loans
   }
   
+  mapping (uint256 => Loan) private loans;
   mapping (address => uint256) private balanceOf;
-  mapping (address => Loan[]) private loans;   // public_key => amount
+  mapping (address => uint256[]) private currentLoans;
+  mapping (address => uint256[]) private loanHistory;
+
+  address private owner;
   string public name;
   string public symbol;
   uint8 public decimals = 18;
@@ -24,11 +47,11 @@ contract Ibcs {
   event Transfer(address indexed from, address indexed to, uint256 value);
   // This notifies clients about the amount burnt
   event Burn(address indexed from, uint256 value);
-
   
   function Ibcs(uint256 initialSupply, string tokenName, string tokenSymbol) {
     totalSupply = initialSupply * 10 ** uint256(decimals);
-    balanceOf[msg.sender] = totalSupply;
+    owner = msg.sender;
+    balanceOf[owner] = totalSupply;
     name = tokenName;
     symbol = tokenSymbol;
   }
