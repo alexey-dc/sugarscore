66d4002d4769a0d56173b7e1a36affe126f29ed7
Alexey Chernikov <iwizem@gmail.com>
2018-01-14 07:26:25 -0800

    W3: endpoints to  connect to the sol script

--- blockchain/deployContract.js
+++ blockchain/deployContract.js
@@ -1,15 +1,15 @@
-const candidates = require('./candidates.js');
 const fs = require('fs');
 const web3 = require('./web3Client.js');
 const code = fs.readFileSync('ibcs.sol').toString();
 const solc = require('solc');
 const compiledCode = solc.compile(code);
-console.log(compiledCode);
 const abiDefinition = JSON.parse(compiledCode.contracts[':Ibcs'].interface);
 
-const VotingContract = web3.eth.contract(abiDefinition);
+const IbcsContract = web3.eth.contract(abiDefinition);
 const byteCode = compiledCode.contracts[':Ibcs'].bytecode;
-const deployedContract = VotingContract.new(candidates, { data: byteCode, from: web3.eth.accounts[0], gas: 4700000 });
+
+const params = {"comment": "this used to be the list of candidates in the sample app"}
+const deployedContract = IbcsContract.new(params, { data: byteCode, from: web3.eth.accounts[0], gas: 4700000 });
 
 module.exports = deployedContract;
 
--- blockchain/server.js
+++ blockchain/server.js
@@ -3,7 +3,6 @@ const contractInstance = require('./deployContract.js');
 const web3 = require('./web3Client.js');
 const app = express();
 const bodyParser = require('body-parser');
-const candidates = require('./candidates.js');
 const path = require('path');
 
 app.use(express.static(path.join(__dirname, 'public')));
@@ -13,34 +12,45 @@ app.get('/', function(req, res){
   res.sendFile(path.join(__dirname + 'public/index.html'));
 });
 
-app.post('/vote', function (req, res) {
-  try {
-    const candidateName = req.body.candidateName.trim();
 
-    contractInstance.voteForCandidate(candidateName, { from: web3.eth.accounts[0] }, function(result) {
-      const totalVotes = contractInstance.totalVotesFor.call(candidateName, { from: web3.eth.accounts[0] }).toString();
-      res.send({ votes: totalVotes, name: candidateName });
-    });
+app.get('/getProfile', function(req, res) {
+  try {
+    var address = req.query.address;
+    var bProfile = contractInstance.getProfile.call(address, web3.eth.accounts[0]);
+    var profile = {
+      currentBalance: bProfile[0].toString(),
+      loanSum: bProfile[1].toString(),
+      reputation: bProfile[2].toString(),
+      borrowLimit: bProfile[3].toString()
+    }
+    res.send(profile)
   } catch (e) {
-    res.status('400').send(`Failed! ${e}`);
+    res.status('400').send(`Failed! ${e}`)
   }
 });
 
-app.get('/candidates', function(req, res) {
-  try {
-    const candidateVotes = candidates.map(function(candidate) {
-      const votes = contractInstance.totalVotesFor.call(candidate, { from: web3.eth.accounts[0] }).toString();
-      return {
-        name: candidate,
-        votes: votes,
-      };
-    });
+app.get('/getLoans', function(req,res) {
+  var address = req.query.address;
+  var unpayedLoanIds = contractInstance.unpayedLoanIds.call(address, web3.eth.accounts[0]);
 
-    res.send({ candidates: candidateVotes });
-  } catch (e) {
-    res.status('400').send(`Failed! ${e}`);
-  }
 });
+
+app.post('/borrow', function(req, res) {
+  var address = req.query.address;
+  var amount = req.query.amount;
+  var ratePercent = req.query.ratePercent;
+  var durationDays = req.query.durationDays;
+  // contractInstance.getProfile(address, { from: web3.eth.accounts[0] }, function(result) {
+  //   res.send({ votes: totalVotes, name: candidateName });
+  // });
+});
+
+app.post('/payBack', function(req, res) {
+  var address = req.query.address;
+  var loanId = req.query.loanId;
+
+});
+
 
 app.listen(3000, function () {
   console.log('App ready and listening on port 3000!')
